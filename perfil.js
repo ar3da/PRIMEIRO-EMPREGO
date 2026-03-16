06ba389ed41a3c2216dba6fcbccde69103e599f1
Areda Joelma <115044541+ar3da@users.noreply.github.com>
2026-03-16 17:41:09 -0300

    Create perfil.js

---
+++ perfil.js
@@ -0,0 +1,15 @@
+let nome = "Areda";
+let idade = 22;
+let cidade = "Campina Grande";
+let formacao = "Análise e Desenvolvimento de Sistemas";
+let experiencia = "Nenhuma experiência profissional na área";
+let objetivo = "Conseguir um emprego que me permita aprender e crescer como programadora, mesmo que seja uma posição de nível inicial ou estágio";
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Cidade: ", cidade);
+console.log("Formação: ", formacao);
+console.log("Experiência: ", experiencia);
+console.log("Objetivo: ", objetivo);
+
+console.log("Sou uma pessoa dedicada, curiosa e apaixonada por tecnologia. Estou sempre buscando aprender coisas novas e me aprimorar como programadora. Acredito que a prática é fundamental para o desenvolvimento de habilidades, por isso estou aberta a oportunidades de estágio ou posições de nível inicial para ganhar experiência e contribuir com projetos interessantes.");
