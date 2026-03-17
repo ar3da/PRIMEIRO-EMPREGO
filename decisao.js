46d70429b4ae708d0947df89f18ac137bbf75528
Areda Joelma <115044541+ar3da@users.noreply.github.com>
2026-03-17 18:19:47 -0300

    Update decisao.js

--- decisao.js
+++ decisao.js
@@ -1,4 +1,24 @@
+let nome = "Areda";
 let idade = 22;
+let experiencia = 0;
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+
+let nivel;
+if (experiencia === 0) {
+    nivel = "Júnior"
+} else if (experiencia > 0 && experiencia <= 2) {
+    nivel = "Pleno";
+} else if (experiencia > 2) {
+    nivel = "Sênior";
+}
+
+console.log("Nível: ", nivel);
+
 if (idade >= 18) {
-    console.log("Você é maior de idade"));
+    console.log("Status: Pode trabalhar");
+} else {
+    console.log("Status: Não pode trabalhar");
+}
     
