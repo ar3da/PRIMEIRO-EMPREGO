c38de525b3ac9944401b1319ca3ea65afc5a5601
Areda Joelma <115044541+ar3da@users.noreply.github.com>
2026-04-02 19:28:59 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -11,3 +11,7 @@ let idades = [22, 30, 22, 28, 35]
 console.log("Idades: ", idades)
 console.log("Primeira idade: ", idades[0])
 console.log("Última idade: ", idades[idades.length - 1])
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
