a76baa866aee625f952f785ec04d296525932377
Areda Joelma <115044541+ar3da@users.noreply.github.com>
2026-04-02 19:14:34 -0300

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let nomes = ["Areda", "Josana", "Milena", "Edilma", "Italo"]
+
+console.log("Nomes: ", nomes)
+console.log("Primeiro nome: ", nomes[0])
+console.log("Último nome: ", nomes[nomes.length - 1])
+
+nomes.push("Maria")
+console.log("Nomes após adicionar Maria: ", nomes)
+
+let idades = [22, 30, 22, 28, 35]
+console.log("Idades: ", idades)
+console.log("Primeira idade: ", idades[0])
+console.log("Última idade: ", idades[idades.length - 1])
